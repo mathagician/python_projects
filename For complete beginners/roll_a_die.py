535eff6931c563dfc9a2edbe9fde48c4c5edcf06
alvinma <alvinma111@gmail.com>
2018-10-02 00:52:39 -0700

    Adding Comments and Else Case
    Adding comments and an else case for invalid inputs when giving the user the option to decide whether to roll the dice again or to quit

--- For complete beginners/roll_a_die.py
+++ For complete beginners/roll_a_die.py
@@ -13,10 +13,12 @@ def get_dice_sides():
 
 if __name__ == '__main__':
     n_sides = get_dice_sides()
-    while True:
+    while True:      # will result with an infinite loop until broken out of with "break"
         typed = input('Roll the dice ("y" to roll, "quit" to quit)? ').lower()
         if typed == 'quit':
             print('Thank you for playing, enjoy your day! :)')
-            break
+            break    # only possible way of breaking out of the infinite loop
         elif typed in ('y', 'yes', 'roll'):
-            print(f'You rolled a {random.randint(1, n_sides)}!')
+            print(f'You rolled a {random.randint(1, n_sides)}!')   # built-in function that will return a random integer between 1 and n-sided die input.
+        else:
+            print('Invalid input! Please enter a valid input.')
