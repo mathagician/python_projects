6af17eccca2418196c60aaff8a24e810a6ed42fe
Uzer Jamal <Uzerjamal@gmail.com>
2018-10-02 13:45:31 +0530

    Moved programs

--- Projects for complete beginners/String Programs/CountingWords.py
+++
@@ -1,7 +0,0 @@
-def count_words(str):
-    return len(str.split())
-
-
-if __name__ == '__main__':
-    str = input('Enter a string\n')
-    print(f'Number of words: {count_words(str)}')
--- Projects for complete beginners/String Programs/ReverseString.py
+++
@@ -1,8 +0,0 @@
-def reverse(str):
-    str = ''.join(reversed(str))
-    return str
-
-
-if __name__ == '__main__':
-    str = input('Enter a string to reverse\n')
-    print(reverse(str))
